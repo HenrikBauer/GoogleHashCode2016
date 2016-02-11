99f6237d0a09612fe6f78cf337ba0efcea55e901
rurarz <olekrurarz@gmail.com>
2016-02-11 19:39:25 +0100

    Order and product class add

---
+++ IntVector2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public struct IntVector2
+{
+    public int x;
+    public int y;
+
+    public IntVector2(int newX, int newY)
+    {
+        x = newX;
+        y = newY;
+    }
+}
+
---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash2016
+{
+    class Product
+    {
+        private int _weight;
+
+        public int GetWeight()
+        {
+            return _weight;
+        }
+    }
+}
